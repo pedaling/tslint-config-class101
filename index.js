2ba5c6104d9bc6cbcaee6aa455f32102bda15d50
john015 <lsw0150305@gmail.com>
2019-10-04 17:47:19 +0900

    feat: import-name의 예외, jsx-boolean-value 룰을 추가한다

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
 module.exports = {
   extends: [
-    "tslint:recommended",
-    "tslint-react",
-    "tslint-config-airbnb",
-    "tslint-config-prettier"
+    'tslint:recommended',
+    'tslint-react',
+    'tslint-config-airbnb',
+    'tslint-config-prettier'
   ],
   rules: {
-    "array-type": [true, "array"],
-    "interface-name": [true, "never-prefix"],
-    "import-name": [
+    'array-type': [true, 'array'],
+    'interface-name': [true, 'never-prefix'],
+    'import-name': [
       true,
       {
-        react: "React",
-        "react-dom": "ReactDom",
-        "styled-components": "styled"
+        react: 'React',
+        reactDom: 'ReactDom',
+        styledComponents: 'styled',
+        graphqlTag: 'gql',
+        reactLoadingSkeleton: 'Skeleton',
+        reactHelmet: 'Helmet',
+        reactEventListener: 'EventListener',
+        ravenJs: 'Raven'
       }
     ],
-    "variable-name": [
+    'variable-name': [
       true,
-      "ban-keywords",
-      "check-format",
-      "allow-pascal-case"
+      'ban-keywords',
+      'check-format',
+      'allow-pascal-case'
     ],
-    "no-namespace": false,
-    "object-literal-sort-keys": false,
-    "ordered-imports": [
+    'no-namespace': false,
+    'object-literal-sort-keys': false,
+    'ordered-imports': [
       true,
       {
-        "named-imports-order": "any"
+        'named-imports-order': 'any'
       }
     ],
-    "no-unused-variable": true,
-    "no-unused-expression": true,
-    quotemark: [true, "single", "jsx-double"]
+    'no-unused-variable': true,
+    'no-unused-expression': true,
+    'jsx-boolean-value': false,
+    quotemark: [true, 'single', 'jsx-double']
   }
-};
+}
