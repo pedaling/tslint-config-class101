7e897827b5e43d97e2d7b384c4e9229ba738cd6e
john015 <lsw0150305@gmail.com>
2019-11-18 20:36:47 +0900

    fix: ]가 빠져서 린트 에러나는 문제 수정

--- index.js
+++ index.js
@@ -8,27 +8,28 @@ module.exports = {
   rules: {
     'array-type': [true, 'array'],
     'interface-name': [true, 'never-prefix'],
-   'import-name': [
-     true,
-     {
-       react: 'React',
-       reactDom: 'ReactDom',
-       styledComponents: 'styled',
-       graphqlTag: 'gql',
-       reactLoadingSkeleton: 'Skeleton',
-       reactHelmet: 'Helmet',
-       reactEventListener: 'EventListener',
-       ravenJs: 'Raven',
-       reactLoadable: 'Loadable',
-       reactRouterBreadcrumbsHoc: 'withBreadcrumbs',
-       bsonObjectid: 'ObjectId',
-       htmlReactParser: 'htmlParser',
-       josaJs: 'josa',
-       reactCountup: 'CountUp',
-       reactAnimateOnScroll: 'ScrollAnimation',
-       reactInfiniteScrollComponent: 'InfiniteScroll',
-       reactLinkify: 'ReactLinkify'
-     },
+    'import-name': [
+      true,
+      {
+        react: 'React',
+        reactDom: 'ReactDom',
+        styledComponents: 'styled',
+        graphqlTag: 'gql',
+        reactLoadingSkeleton: 'Skeleton',
+        reactHelmet: 'Helmet',
+        reactEventListener: 'EventListener',
+        ravenJs: 'Raven',
+        reactLoadable: 'Loadable',
+        reactRouterBreadcrumbsHoc: 'withBreadcrumbs',
+        bsonObjectid: 'ObjectId',
+        htmlReactParser: 'htmlParser',
+        josaJs: 'josa',
+        reactCountup: 'CountUp',
+        reactAnimateOnScroll: 'ScrollAnimation',
+        reactInfiniteScrollComponent: 'InfiniteScroll',
+        reactLinkify: 'ReactLinkify'
+      },
+    ],
     'variable-name': [
       true,
       'ban-keywords',
@@ -46,6 +47,6 @@ module.exports = {
     'no-unused-variable': true,
     'no-unused-expression': true,
     'jsx-boolean-value': false,
-    quotemark: [true, 'single', 'jsx-double']
+    'quotemark': [true, 'single', 'jsx-double']
   }
 }
